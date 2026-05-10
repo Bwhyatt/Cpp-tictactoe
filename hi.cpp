72e59e5c177329b0408e7e84798a333fa8a902e0
Bwhyatt <brandon.whyatt@outlook.com>
2026-05-10 20:37:09 +1000

    so i made it go back and forth with delta time incorporated

--- hi.cpp
+++ hi.cpp
@@ -6,30 +6,45 @@ int main(int argc, char* argv[]) {
 
     SDL_Window* window = SDL_CreateWindow(
         "My Window",          // title
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,  // position
-        800, 600,             // width, height
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,  // position
+        800,
+        600,             // width, height
         SDL_WINDOW_SHOWN
     );
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     // main loop
+     SDL_Rect myRect = { 50, 50, 200, 150 };
     bool running = true;
     SDL_Event event;
-
+    float dir = 1;
+    Uint32 last_time = 0, current_time = 0;
+    float delta_time = 0;
+    float speed = 1000.0f;
     while (running) {
+        current_time = SDL_GetTicks();
+        // delta_time in seconds
+        delta_time = static_cast<float>(current_time - last_time)/1000.0f;
+        last_time = current_time;
+
         // handle events
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) running = false;
             if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) running = false;
         }
-
+        if (myRect.x + myRect.w > SDL_GetWindowSurface(window)->w || myRect.x < 0) dir *= -1;
+        myRect.x += static_cast<int>(dir * delta_time * speed);
+        std::cout << myRect.x << std::endl;
         // clear screen (dark blue)
         SDL_SetRenderDrawColor(renderer, 20, 20, 40, 255);
         SDL_RenderClear(renderer);
 
-        // draw stuff here later...
 
+        // draw stuff here later...
+        SDL_SetRenderDrawColor(renderer, 100, 20, 40, 255);
+        SDL_RenderFillRect(renderer, &myRect);
         SDL_RenderPresent(renderer);
     }
 
